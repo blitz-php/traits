0be6976e203d45aa032ff5664755b1950d15cf07
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-10-31 16:16:02 +0100

    feat: trait InteractWithTime

---
+++ Support/InteractsWithTime.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Traits\Support;
+
+use BlitzPHP\Utilities\Date;
+use DateInterval;
+use DateTimeInterface;
+
+trait InteractsWithTime
+{
+    /**
+     * Obtenez le nombre de secondes jusqu'au DateTime donné.
+     */
+    protected function secondsUntil(DateTimeInterface|DateInterval|int $delay): int
+    {
+        $delay = $this->parseDateInterval($delay);
+
+        return $delay instanceof DateTimeInterface
+                            ? max(0, $delay->getTimestamp() - $this->currentTime())
+                            : (int) $delay;
+    }
+
+    /**
+     * Obtenez l'horodatage UNIX "disponible à".
+     */
+    protected function availableAt(DateTimeInterface|DateInterval|int $delay = 0): int
+    {
+        $delay = $this->parseDateInterval($delay);
+
+        return $delay instanceof DateTimeInterface
+                            ? $delay->getTimestamp()
+                            : Date::now()->addSeconds($delay)->getTimestamp();
+    }
+
+    /**
+     * Si la valeur donnée est un intervalle, convertissez-la en instance DateTime.
+     *
+     * @return DateTimeInterface|int
+     */
+    protected function parseDateInterval(DateTimeInterface|DateInterval|int $delay)
+    {
+        if ($delay instanceof DateInterval) {
+            $delay = Date::now()->add($delay);
+        }
+
+        return $delay;
+    }
+
+    /**
+     * Obtenez l’heure actuelle du système sous forme d’horodatage UNIX.
+     */
+    protected function currentTime(): int
+    {
+        return Date::now()->getTimestamp();
+    }
+}
